0a3fbf1ed4970c755b6f98a59e9f7fa09ae36512
JALWilliams <just.williams04@gmail.com>
2022-02-03 10:16:10 +0000

    User story 7 compelted

--- spec/officeSpec.js
+++ spec/officeSpec.js
@@ -1,19 +1,31 @@
 describe ( 'Office', ()=>{
 
   let ft;
+  let lantana;
+  let plaza;
 
   beforeEach(()=>{
     ft = new office ();
+    lantana = new meetingRoom('Lantana');
+    plaza = new meetingRoom('Plaza')
   });
 
   it ('Add meeting room to office', ()=>{
-    ft.addMeetingRoom('Lantana')
-    expect(ft.meetingRooms).toEqual(['Lantana'])
+    ft.addMeetingRoom(lantana);
+    expect(ft.meetingRooms).toEqual(['Lantana']);
   });
 
   it ('List the meeting rooms', ()=>{
-    ft.addMeetingRoom('Lantana')
-    expect(ft.viewMeetingRooms()).toEqual(['Lantana'])
+    ft.addMeetingRoom(lantana);
+    expect(ft.viewMeetingRooms()).toEqual(['Lantana']);
+  });
+
+  it ('List of available rooms', ()=>{
+    ft.addMeetingRoom(lantana);
+    ft.addMeetingRoom(plaza);
+    lantana.enter();
+
+    expect(ft.viewAvailableMeetingRooms()).toEqual(['Plaza']);
 
   });
 
--- src/office.js
+++ src/office.js
@@ -1,19 +1,38 @@
 class office {
 
   constructor(){
+    this.meetingRoomsInstances = [];
     this.meetingRooms = [];
+    this.availableRooms = [];
   }
 
-  addMeetingRoom(name){
-    return this.meetingRooms.push(name);
+  
+  addMeetingRoom(instanceRoom2){
+    // here we are getting instance
+    this.meetingRoomsInstances.push(instanceRoom2);
+    // here we are just getting the name for the lsit 
+    this.meetingRooms.push(instanceRoom2.name); 
+    // return this.meetingRooms;
   }
 
   viewMeetingRooms(){
     return this.meetingRooms;
   }
 
-  enter(){
-    return 
+  viewAvailableMeetingRooms(){
+
+      this.availableRooms = [];
+      
+      this.meetingRoomsInstances.forEach((room)=>{
+        if (room.availabilityStatus){
+          return this.availableRooms.push(room.name);
+        }
+        if (!room.availabilityStatus){
+          return;
+        }
+      });
+
+      return this.availableRooms;
   }
 
 }
